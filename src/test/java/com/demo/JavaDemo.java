67e265e3770eea1d85d63b82148f8f5dbcd1ceff
LightManA <AwarenessLightA@gmail.com>
2022-04-01 23:23:24 +0600

    Initial commit

---
+++ src/test/java/com/demo/JavaDemo.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JavaDemo {
+
+    @Test
+    void shouldOpen () {
+
+        // LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy"));  // пример ручного написания
+
+        // Создание с помощью рефакторинга экстракт - Ctrl+Alt+V (у меня не рабоате пока)
+        // LocalDate parsedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        // Postfix Completion 23
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy"); // todo LiMa
+        LocalDate localDate = LocalDate.parse("25.03.2022", formatter);
+
+        assertEquals(DayOfWeek.FRIDAY, localDate.getDayOfWeek());
+
+
+    }
+
+
+}
